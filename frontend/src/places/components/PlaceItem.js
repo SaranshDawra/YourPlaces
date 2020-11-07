1ea0f2fe25da3e3e54ffbefb911627e9b194c884
SaranshDawra <saransh2000dawra@gmail.com>
2020-11-07 18:29:23 +0530

    minor bug fixes

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -26,7 +26,7 @@ const PlaceItem = (props) => {
         setShowConfirmModal(false);
         try {
             await sendRequest(
-                `http://localhost:5000/api/places/${props.id}`,
+                `/api/places/${props.id}`,
                 "DELETE",
                 null,
                 {
@@ -66,7 +66,7 @@ const PlaceItem = (props) => {
                     {isLoading && <LoadingSpinner asOverlay />}
                     <div className="place-item__image">
                         <img
-                            src={`http://localhost:5000/${props.image}`}
+                            src={`/${props.image}`}
                             alt={props.title}
                         />
                     </div>
