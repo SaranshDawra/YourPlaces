52b8a3542ebb090b7ca9a3afc63bba43f77d3801
SaranshDawra <saransh2000dawra@gmail.com>
2020-11-07 18:18:23 +0530

    deployment prep

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.use((error, req, res, next) => {
 const port = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
 
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+
 mongoose
     .connect(uri, {
         useNewUrlParser: true,
